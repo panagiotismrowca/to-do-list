18fdc072e5bdd5ab8b1c150889bc65ea9f75d666
Mrowca-Panagiotis <mc.panos.03@gmail.com>
2023-06-11 04:11:39 +0300

    Add new features

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,16 @@
 const addTaskButton = document.querySelector('#addTaskButton');
 const addTaskDiv = document.querySelector('#addTaskDiv');
+const cancelButton = document.querySelector('#cancelButton');
+const exitDiv = document.querySelector('#exitDiv');
 
 const openAddTaskDiv = () => {
   addTaskDiv.style.display = 'block';
+  addTaskDiv.style.transitionDuration = '300ms';
+}
+const openExitDiv = () => {
+  exitDiv.style.display = 'block';
+  exitDiv.style.transitionDuration = '300ms';
 }
 
 addTaskButton.addEventListener('click',openAddTaskDiv);
+cancelButton.addEventListener('click',openExitDiv);
